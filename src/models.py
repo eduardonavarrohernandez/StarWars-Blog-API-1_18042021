a0e45fb23e7dcad9457f8b5bc97a18b617d10340
Roy Mora Mora <rmoramo1@gmail.com>
2021-04-11 02:57:23 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,11 +2,45 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    mail = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+
+    id = Column(Integer, primary_key=True)
+
+    User_id = Column(Integer, ForeignKey('user.id'))
+    User = relationship(User)
+    tipoFavorito = Column(String(250), nullable=False)
+    favoritoId = Column(String(250), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
